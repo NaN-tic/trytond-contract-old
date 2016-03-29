6e68109d0d428ffa88bc342cc115e7db7b17fd21
resteve <resteve@zikzakmedia.com>
2016-03-29 23:44:03 +0200

    Add python 3 support

--- invoice.py
+++ invoice.py
@@ -8,11 +8,11 @@ from trytond.wizard import Wizard, StateView, StateAction, Button
 
 __all__ = ['InvoiceLine', 'CreateInvoicesStart', 'CreateInvoices',
     'CreditInvoiceStart', 'CreditInvoice']
-__metaclass__ = PoolMeta
 
 
 class InvoiceLine:
     __name__ = 'account.invoice.line'
+    __metaclass__ = PoolMeta
 
     @classmethod
     def _get_origin(cls):
@@ -61,6 +61,8 @@ class CreateInvoices(Wizard):
 
 class CreditInvoiceStart:
     __name__ = 'account.invoice.credit.start'
+    __metaclass__ = PoolMeta
+
     from_contract = fields.Boolean('From Contract', readonly=True)
     reinvoice_contract = fields.Boolean('Reinvoice Contract',
         states={
@@ -82,6 +84,7 @@ class CreditInvoiceStart:
 
 class CreditInvoice:
     __name__ = 'account.invoice.credit'
+    __metaclass__ = PoolMeta
 
     def default_start(self, fields):
         pool = Pool()
--- tests/test_contract.py
+++ tests/test_contract.py
@@ -5,6 +5,7 @@ import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
 class TestContractCase(ModuleTestCase):
@@ -19,21 +20,26 @@ def suite():
     suite.addTests(doctest.DocFileSuite(
             'scenario_monthly_full_month_contract.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     suite.addTests(doctest.DocFileSuite(
             'scenario_monthly_start_in_month_contract.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     suite.addTests(doctest.DocFileSuite(
             'scenario_monthly_natural_days_contract.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     suite.addTests(doctest.DocFileSuite(
             'scenario_monthly_full_month_with_invoice_date_contract.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     suite.addTests(doctest.DocFileSuite(
             'scenario_contract_grouping.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
